bd20dc44d15ca574086356a9797927197292e56b
koheishoji <koheishoji913@gmail.com>
2017-08-31 17:41:03 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# fish
+ln -sf ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
+
+# NeoVIM
+ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/.config/nvim/dein.toml ~/.config/nvim/dein.toml
+ln -sf ~/dotfiles/.config/nvim/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
+
+# tmux
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
