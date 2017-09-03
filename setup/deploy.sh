82deb5fdefdde001d6411f5d685263d021dcf35a
koheishoji <koheishoji913@gmail.com>
2017-09-03 21:40:12 +0900

    clone dotfiles

--- setup/deploy.sh
+++ setup/deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# dotfiles
+git clone https://github.com/koheishoji/dotfiles.git ~/dotfiles
+
 # fish
+mkdir "$HOME/.config/fish"
 ln -sf ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
 
 # NeoVIM
+mkdir "$HOME/.config/nvim"
 ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.config/nvim/dein.toml ~/.config/nvim/dein.toml
 ln -sf ~/dotfiles/.config/nvim/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
