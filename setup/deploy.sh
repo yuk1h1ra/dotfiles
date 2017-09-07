3bd550bdd9b4182a86383629eb297029ea5d88b6
koheishoji <koheishoji913@gmail.com>
2017-09-08 08:24:12 +0900

    add symbolic with emacs

--- setup/deploy.sh
+++ setup/deploy.sh
@@ -13,6 +13,9 @@ ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.config/nvim/dein.toml ~/.config/nvim/dein.toml
 ln -sf ~/dotfiles/.config/nvim/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
 
+# emacs
+ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
+
 # tmux
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
