c4b2087203ab8e74dc12c7ae0b83928c0890814d
yuk1h1ra <koheishoji913@gmail.com>
2017-11-20 17:50:35 +0900

    add path about mybin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,9 @@
+# pyenv setting
 set -x PATH $HOME/.pyenv/bin $PATH
 . (pyenv init - | psub)
 
+# eclipsesetting
 set -x CLASSPATH $HOME/Java/GJVim/bin/GJVim $CLASSPATH
 
+# mybin setting
+set -x PATH $HOME/mybin $PATH
