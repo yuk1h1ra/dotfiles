0b6f3b22140acbcebd33473c5f7648da306ac66c
yuk1h1ra <koheishoji913@gmail.com>
2017-12-05 14:07:01 +0900

    delete classpath

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,5 @@
 set -x PATH $HOME/.pyenv/bin $PATH
 . (pyenv init - | psub)
 
-# eclipsesetting
-set -x CLASSPATH $HOME/Java/GJVim/bin/GJVim $CLASSPATH
-
 # mybin setting
 set -x PATH $HOME/mybin $PATH
