3bc78a25a731656cbd91ea5faff77eefbedd8a0b
yuk1h1ra <koheishoji913@gmail.com>
2019-06-20 13:33:50 +0900

    add link script

---
+++ setup/dotfilesLink.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -sf ~/Repositories/github.com/yuk1h1ra/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/Repositories/github.com/yuk1h1ra/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/Repositories/github.com/yuk1h1ra/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/Repositories/github.com/yuk1h1ra/dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
