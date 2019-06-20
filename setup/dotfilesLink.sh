0730168724c8554fd41d25fcd191b88c32658067
yuk1h1ra <koheishoji913@gmail.com>
2019-06-20 18:54:09 +0900

    change ghq root Repositories to src

--- setup/dotfilesLink.sh
+++ setup/dotfilesLink.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ln -sf ~/Repositories/github.com/yuk1h1ra/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/Repositories/github.com/yuk1h1ra/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/Repositories/github.com/yuk1h1ra/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/Repositories/github.com/yuk1h1ra/dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf ~/src/github.com/yuk1h1ra/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/src/github.com/yuk1h1ra/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/src/github.com/yuk1h1ra/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/src/github.com/yuk1h1ra/dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
