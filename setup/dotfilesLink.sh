efcdd95e75539b2aed4d6c9d54222a70270508f3
yuk1h1ra <koheishoji913@gmail.com>
2019-07-06 00:48:31 +0900

    remove cheatsheet link

--- setup/dotfilesLink.sh
+++ setup/dotfilesLink.sh
@@ -3,4 +3,3 @@ ln -sf ~/src/github.com/yuk1h1ra/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/src/github.com/yuk1h1ra/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/src/github.com/yuk1h1ra/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/src/github.com/yuk1h1ra/dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -sf ~/src/github.com/yuk1h1ra/dotfiles/.cheatsheet.md ~/.cheatsheet.md
