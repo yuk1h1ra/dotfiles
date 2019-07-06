bc59a5310369c43d206ef4b13e95ee43be02f84a
yuk1h1ra <koheishoji913@gmail.com>
2019-07-06 18:16:13 +0900

    bug fixed

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,5 +2,5 @@
 
 DOTFILE_SETUP_PATH="$(cd $(dirname $0) && pwd)"
 
-source VimPlugInstallation.sh
-source SymbolicLink.sh
+source "$DOTFILE_SETUP_PATH/VimPlugInstallation.sh"
+source "$DOTFILE_SETUP_PATH/SymbolicLink.sh"
