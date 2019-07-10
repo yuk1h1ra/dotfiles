3d3197cc6b701c00244af0ee08e7e48e6ae23e13
yuk1h1ra <koheishoji913@gmail.com>
2019-07-10 22:15:09 +0900

    add rep debug

--- .config/nvim/template/cpp/base-compe.cpp
+++ .config/nvim/template/cpp/base-compe.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define debug(a) cout << #a << ": " << a << endl;
+
 int main() {
   {{_cursor_}}
 }
