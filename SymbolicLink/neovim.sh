aedec78f7ed58636cac56adbd6b58fd9b802954e
yuk1h1ra <koheishoji913@gmail.com>
2021-03-04 10:16:45 +0900

    Add vim-plug install script

--- SymbolicLink/neovim.sh
+++ SymbolicLink/neovim.sh
@@ -2,7 +2,14 @@
 
 DOTFILE_PATH="$(cd $(dirname $0)/.. && pwd)"
 
+# Symbolic init.vim
 if [ ! -d "$HOME/.config/nvim" ]; then
   mkdir -p "$HOME/.config/nvim"
 fi
 ln -snfv "$DOTFILE_PATH/.config/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+
+# Install vim-plug
+if [ ! -d "$HOME/.local/share/nvim/site/autoload/plug.vim" ]; then
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fi
