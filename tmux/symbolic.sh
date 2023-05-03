0803984a087cc5ebc90f1f48106c3bfbce48275c
yuk1h1ra <iam.yuk1h1ra.me@gmail.com>
2023-05-03 10:25:01 +0900

    Change tmux symbolic file

--- tmux/symbolic.sh
+++ tmux/symbolic.sh
@@ -6,7 +6,7 @@ ln -snfv "$DOTFILE_PATH/tmux/.tmux.conf" "$HOME/.tmux.conf"
 
 # tmux-powerline
 ln -snfv "$DOTFILE_PATH/tmux/.tmux-powerlinerc" "$HOME/.tmux-powerlinerc"
-if [ -d ~/.config/tmux-powerline/themes ]; then
+if [ ! -d ~/.config/tmux-powerline/themes ]; then
   mkdir -p ~/.config/tmux-powerline/themes
 fi
 ln -snfv "$DOTFILE_PATH/tmux/.config/tmux-powerline/themes/simple.sh" "$HOME/.config/tmux-powerline/themes/simple.sh"
