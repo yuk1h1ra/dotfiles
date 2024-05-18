6262a823ae52fef11e4b68fe6679b973c11cf752
yuk1h1ra <koheishoji913@gmail.com>
2024-05-19 07:32:45 +0900

    Change tmux

--- tmux/symbolic.sh
+++ tmux/symbolic.sh
@@ -4,6 +4,9 @@ DOTFILE_PATH="$(cd $(dirname $0)/.. && pwd)"
 
 ln -snfv "$DOTFILE_PATH/tmux/.tmux.conf" "$HOME/.tmux.conf"
 
+# tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 # tmux-powerline
 ln -snfv "$DOTFILE_PATH/tmux/.tmux-powerlinerc" "$HOME/.tmux-powerlinerc"
 if [ ! -d ~/.config/tmux-powerline/themes ]; then
