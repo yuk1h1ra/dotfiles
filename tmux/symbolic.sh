7e391ce27f1abc72ac4769275222372e4d64e547
yuk1h1ra <iam.yuk1h1ra.me@gmail.com>
2025-01-11 10:15:13 +0900

    Update symbolic.sh

--- tmux/symbolic.sh
+++ tmux/symbolic.sh
@@ -8,8 +8,8 @@ ln -snfv "$DOTFILE_PATH/tmux/.tmux.conf" "$HOME/.tmux.conf"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # tmux-powerline
-ln -snfv "$DOTFILE_PATH/tmux/.tmux-powerlinerc" "$HOME/.tmux-powerlinerc"
 if [ ! -d ~/.config/tmux-powerline/themes ]; then
   mkdir -p ~/.config/tmux-powerline/themes
 fi
+ln -snfv "$DOTFILE_PATH/tmux/.config/config.sh" "$HOME/.config/tmux-powerline/config.sh"
 ln -snfv "$DOTFILE_PATH/tmux/.config/tmux-powerline/themes/simple.sh" "$HOME/.config/tmux-powerline/themes/simple.sh"
