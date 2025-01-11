90bb8fa2f326c4c22297ed506027c22b53ed98f2
yuk1h1ra <iam.yuk1h1ra.me@gmail.com>
2025-01-11 10:17:13 +0900

    Update symbolic.sh

--- tmux/symbolic.sh
+++ tmux/symbolic.sh
@@ -5,7 +5,9 @@ DOTFILE_PATH="$(cd $(dirname $0)/.. && pwd)"
 ln -snfv "$DOTFILE_PATH/tmux/.tmux.conf" "$HOME/.tmux.conf"
 
 # tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 
 # tmux-powerline
 if [ ! -d ~/.config/tmux-powerline/themes ]; then
